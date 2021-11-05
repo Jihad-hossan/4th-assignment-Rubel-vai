d391ba79bc88e7fc673fd3fc64719e4f362fea03
Jihad <jihad.marine@gmail.com>
2021-11-05 10:48:23 +0600

    responsive for tablet

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.hambarger').click(function(){
+        $('.navbar1').toggle();
+    });
+    $('.cancel').click(function(){
+        $('.navbar1').hide();
+    })
+})
